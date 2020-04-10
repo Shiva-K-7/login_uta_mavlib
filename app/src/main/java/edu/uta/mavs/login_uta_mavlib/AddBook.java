4a104aa8250e82bd5cdc1b0ee7064c30dc778a5e
Shiva-K-7 <56608595+Shiva-K-7@users.noreply.github.com>
2020-04-09 23:29:41 -0500

    all UIs are updated
    UIs and screen flow is updated and is better now

---
+++ app/src/main/java/edu/uta/mavs/login_uta_mavlib/AddBook.java
@@ -0,0 +1,16 @@
+package edu.uta.mavs.login_uta_mavlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+
+
+    }
+}
---
+++ app/src/main/java/edu/uta/mavs/login_uta_mavlib/ManageBooks.java
@@ -0,0 +1,38 @@
+package edu.uta.mavs.login_uta_mavlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ManageBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_books);
+
+        final Button add_book = findViewById(R.id.add_books);
+        final Button remove_book = findViewById(R.id.remove_books);
+
+        add_book.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i2=new Intent(ManageBooks.this,AddBook.class);
+                startActivity(i2);
+                //setContentView(R.layout.activity_add_book);
+            }
+        });
+
+        remove_book.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(ManageBooks.this,DeleteBook.class);
+                startActivity(i);
+                //setContentView(R.layout.activity_search_books);
+            }
+        });
+    }
+}
